8aaebe193703d8fa758aa02691f6948b7aa6770c
krzych7756 <128181858+krzych7756@users.noreply.github.com>
2024-06-11 00:06:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phoneNumber = htmlspecialchars($_POST['phoneNumber']);
 
-    $to = "twoj-email@example.com"; // Zmień na swój adres e-mail
+    $to = "Necroton18@gmail.com"; // Zmień na swój adres e-mail
     $subject = "Prośba o kontakt";
     $body = "Potencjalny klient chciałby porozmawiać o lockersie. Numer telefonu: $phoneNumber";
     $headers = "From: no-reply@example.com"; // Zmień na odpowiedni adres
